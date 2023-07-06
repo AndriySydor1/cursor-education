51a1229ca108ac1df79f5face7b5a1b6989a2b6b
Andriy <AndriySydor91@gmail.com>
2023-07-06 19:31:54 +0300

    hw2 changed

--- homework2/hw2CyclesAndBranching.js
+++ homework2/hw2CyclesAndBranching.js
@@ -1,34 +1,25 @@
-let N = Number(prompt("Введіть число N", ""));
-if (Number.isInteger(N)) {
-  console.log("Число N ціле:", N, true);
-} else if (isNaN(N) || !Number.isInteger(N)) {
-  N = prompt("Повторно введіть значення числа N.", "");
+let N = Number(prompt("Введіть число N"));
+while (!Number.isInteger(N)) {
+  N = Number(prompt("Повторно введіть значення числа N"));
 }
-let M = Number(prompt("Введіть число M", ""));
-if (isNaN(M) || M < N || !Number.isInteger(M)) {
-  M = prompt("Повторно введіть значення числа M.");
+console.log(`Число N` + N);
+let M = Number(prompt("Введіть число M"));
+while (!Number.isInteger(M) || M < N) {
+  M = Number(prompt("Повторно введіть значення числа M"));
 }
-if (Math.trunc(M) === M) {
-  console.log("Число M ціле:", M, true);
-}
-let access = confirm("Чи потрібно пропускати парні числа?");
-switch (access) {
-  case true:
-    {
-      console.log("Пропускаємо парні числа.");
-    }
-    break;
-  case false: {
-    console.log("Не пропускаємо парні числа.");
-  }
-}
+console.log(`Число M` + M);
+let missEven = confirm("Чи потрібно пропускати парні числа?");
 let sum = 0;
 for (let i = N; i <= M; i++) {
-  if (access == false) {
-    sum = sum + i;
-  } else if (access == true && i % 2 == 1) {
-    sum = sum + i;
+  if (missEven && i % 2 == 0) {
   }
+  sum += i;
 }
-console.log("Сума чисел від N до M:", sum);
-alert(sum);
+console.log(sum);
+document.writeln(
+  `Число N: ${N} <br>
+    Число M: ${M} <br>
+    Чи пропускаємо парні числа? ${missEven} <br>
+    Сума: ${sum}
+    `
+);
