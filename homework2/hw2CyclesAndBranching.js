18b2b9d2032b200f6c02a49e3404bdab5e34f5df
Andriy <AndriySydor91@gmail.com>
2023-07-07 21:54:42 +0300

    HW2 new correction

--- homework2/hw2CyclesAndBranching.js
+++ homework2/hw2CyclesAndBranching.js
@@ -2,12 +2,10 @@ let N = Number(prompt("Введіть число N"));
 while (!Number.isInteger(N)) {
   N = Number(prompt("Повторно введіть значення числа N"));
 }
-console.log(`Число N` + N);
 let M = Number(prompt("Введіть число M"));
 while (!Number.isInteger(M) || M < N) {
   M = Number(prompt("Повторно введіть значення числа M"));
 }
-console.log(`Число M` + M);
 let missEven = confirm("Чи потрібно пропускати парні числа?");
 let sum = 0;
 for (let i = N; i <= M; i++) {
@@ -19,7 +17,6 @@ for (let i = N; i <= M; i++) {
     sum += i;
   }
 }
-console.log(sum);
 document.writeln(
   `Число N: ${N} <br>
     Число M: ${M} <br>
